5480e167106c8936566db9bbdcce72f2b91bbcc6
Mendoza210727 <mendoza.mc27@gemail.com>
2025-06-26 13:11:09 -0400

    cambio3

--- service-worker.js
+++ service-worker.js
@@ -2,25 +2,16 @@ self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open("codecloud-cache").then(function (cache) {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./assets/css/creative-design.css",
-        "./assets/js/creative-design.js",
-        "./assets/js/chatbot.js",
-        "./assets/vendors/jquery/jquery-3.4.1.js",
-        "./assets/vendors/bootstrap/bootstrap.bundle.js",
-        "./assets/vendors/bootstrap/bootstrap.affix.js",
-        "./assets/imgs/logo_code_cloud.png",
-        "./assets/imgs/logo_code_cloud.png"
+        "/",
+        "/index.html",
+        "/assets/css/creative-design.css",
+        "/assets/js/creative-design.js",
+        "/assets/js/chatbot.js",
+        "/assets/vendors/jquery/jquery-3.4.1.js",
+        "/assets/vendors/bootstrap/bootstrap.bundle.js",
+        "/assets/vendors/bootstrap/bootstrap.affix.js",
+        "/assets/imgs/logo_code_cloud.png"
       ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
     })
   );
 });
